d60ae5fdf2930d00e59325f29a78a92f3749ed89
FrancisKiarieM <frankiazh@gmail.com>
2022-07-04 11:44:34 -0700

    putchar

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,9 @@
 
 int main(void)
 
-{
-int ch;
-for (ch=97;ch>=122;ch++)
-{putchar(ch);
-}
-putchar("\");
-return (0);
+{char a;
+for (a ='a'; a <= 'z' ;a++)
+putchar(a);
+putchar('\n');
+return (0)
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 assign a random number to the variable n each time it is executed
+a program that prints the alphabet in lowercase, followed by a new line.

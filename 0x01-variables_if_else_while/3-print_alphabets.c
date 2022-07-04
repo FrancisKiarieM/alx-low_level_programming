3cda6f8e74a0289ccfc8ea397c88a723c7d8bdf5
FrancisKiarieM <frankiazh@gmail.com>
2022-07-04 13:39:50 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,15 @@
 */
 int main(void)
 {
-char a;
-for (a = 'a'; a <= 'z'; a++)
-putchar(a);
-for (a = 'A'; a <= 'Z'; a++)
-putchar(a);
-putchar('\n');
+int ch;
+for (ch = 97; ch <= 122; ch++)
+{
+putchar(ch);
+}
+for (ch = 65; ch <= 90; ch++)
+{
+putchar(ch);
+}
+putchar(10);
 return (0);
 }

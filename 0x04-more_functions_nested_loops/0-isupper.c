fffb6f1a4e06955307626850940888377fc52c83
FrancisKiarieM <frankiazh@gmail.com>
2022-07-10 04:28:19 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,17 +5,21 @@
 /**
  * _isupper -> this is a function to check upper case
  * @c: an integer paramer
- * Return: something but success
+ * Return: give 1 0r 0
  */
 
 int _isupper(int c)
 
 {
-return (c >= 'A' && c <= 'Z');
+if (c >= 'A' && c <= 'Z')
+
 {
+
 return (1);
 }
+
 else
+
 {
 return (0);
 }
